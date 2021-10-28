bff8e97586f31cad7038bbb7e5e37eacbfd9de95
timothy22000 <timothy22000@gmail.com>
2021-10-28 13:57:50 +0100

    Update setup files

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     test_suite='tests',
     tests_require=read("requirements.txt"),
     url='https://github.com/osirixgrpc/pyosirix',
-    version='0.1.2',
+    version='0.1.4',
     zip_safe=False,
 )
